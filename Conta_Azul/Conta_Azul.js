7fcb360b54293932925c0328fd3b39d97a279eab
MaxwellMGomes <maxwell.gomes@telefonica.com>
2026-05-11 13:42:08 -0300

    await

--- Conta_Azul/Conta_Azul.js
+++ Conta_Azul/Conta_Azul.js
@@ -63,9 +63,9 @@ btGeraCod.addEventListener('click', (event) => {
 
 })
 
-btGeraToken.addEventListener('click', (event) => {
+btGeraToken.addEventListener('click', async(event) => {
     event.preventDefault()
-    let token_todos = gera_token()
+    const token_todos = await gera_token()
     cb_token_acesso.value = token_todos.access_token
     cb_token_renova.value = token_todos.refresh_token
     console.log(token_todos)
